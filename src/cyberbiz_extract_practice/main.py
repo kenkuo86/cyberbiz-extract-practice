0e564639812a543bf619d057945d4a27c81bcab3
郭謙 <guoqian@guoqiandeMacBook-Air.local>
2026-07-29 18:06:21 +0800

    module 2: editing request time with datetime module

--- src/cyberbiz_extract_practice/main.py
+++ src/cyberbiz_extract_practice/main.py
@@ -1,4 +1,6 @@
 import time
+from datetime import datetime, timezone
+from email import utils
 import hmac
 import hashlib
 import base64
@@ -17,36 +19,42 @@ headers = 'x-date request-line'
 url = url_base + url_path
 
 # x-date
+# 參考範例：'Tue, 10 Oct 2017 00:00:00 GMT'
 x_date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime())
 print('===== x-date =====')
 print(x_date)
 print('==========')
 
-# request-line
-rline = http_method + ' ' + url_path + ' HTTP/1.1'
-print('===== request-line =====')
-print(rline)
-print('==========')
+# x_date_datetime = datetime.now(tz=timezone.utc).strftime('%a, %d %b %Y %H:%M:%S %Z')
+x_date_datetime = utils.format_datetime(datetime.now(tz=timezone.utc), usegmt=True)
+print('===== x-date-datetime =====')
+print(x_date_datetime)
 
-# payload
-payload = 'page=1&per_page=1&offset=0'
+# # request-line
+# rline = http_method + ' ' + url_path + ' HTTP/1.1'
+# print('===== request-line =====')
+# print(rline)
+# print('==========')
 
-# sig_str
-sig_str = 'x-date: ' + x_date + '\n' + rline
-print('===== sig_str =====')
-print(sig_str)
-print('==========')
+# # payload
+# payload = 'page=1&per_page=1&offset=0'
 
-# authorization
-dig = hmac.new(secret, msg=sig_str.encode(), digestmod=hashlib.sha256).digest()
-sig = base64.b64encode(dig).decode()
-auth = 'hmac username="' + username + '", algorithm="hmac-sha256", headers="' + headers + '", signature="' + sig + '"'
-print('===== authorization =====')
-print(auth)
-print('==========')
+# # sig_str
+# sig_str = 'x-date: ' + x_date + '\n' + rline
+# print('===== sig_str =====')
+# print(sig_str)
+# print('==========')
 
-request_headers = {'X-Date': x_date, 'Authorization': auth}
-r = requests.get(url, headers=request_headers, params=payload)
-print('===== Done ' + http_method + ' =====')
-print(r.text)
-print('==========')
+# # authorization
+# dig = hmac.new(secret, msg=sig_str.encode(), digestmod=hashlib.sha256).digest()
+# sig = base64.b64encode(dig).decode()
+# auth = 'hmac username="' + username + '", algorithm="hmac-sha256", headers="' + headers + '", signature="' + sig + '"'
+# print('===== authorization =====')
+# print(auth)
+# print('==========')
+
+# request_headers = {'X-Date': x_date, 'Authorization': auth}
+# r = requests.get(url, headers=request_headers, params=payload)
+# print('===== Done ' + http_method + ' =====')
+# print(r.text)
+# print('==========')
