d976faa3adc1854ceaa088c2449c6d51705eda5a
郭謙 <guoqian@guoqiandeMacBook-Air.local>
2026-08-05 19:01:24 +0800

    module 3: Step A completed

--- src/cyberbiz_extract_practice/main.py
+++ src/cyberbiz_extract_practice/main.py
@@ -9,24 +9,6 @@ from requests.auth import AuthBase
 username = 'apidemo'
 secret = b'apidemo' # 最前面的 b 的效果是把後面的字串轉成 bytes
 
-# api url
-url_base = 'https://api.cyberbiz.co'
-url_path = '/v1/orders'
-url = url_base + url_path
-
-# payload
-payload = 'page=1&per_page=1&offset=0'
-
-# 讓 Session 的 Auth 可以隨著每次 request 更改必要的值
-# 引入 AuthBase：可以在送出 request 前修改 request 的內容（看 __call__：輸入 r，回傳 r）
-# 要修改哪些內容，就會需要我們在專用的 CbzAuth 裡面指定
-# 延續 Session 的精神：可以共用的東西只呼叫一次，需要去回頭看 request 裡面有哪些東西是多次呼叫中一樣 & 不一樣的
-# 一樣的就放 __init__，不一樣的就放 __call__
-
-# 單次呼叫的標準內容：requests.get(url, headers=request_headers, params=payload)
-# auth = 'hmac username="' + username + '", algorithm="hmac-sha256", headers="' + headers + '", signature="' + sig + '"'
-# headers = 'x-date request-line'
-
 class CbzAuth(AuthBase):
     def __init__(self, username, secret):
        # 只放跟 auth 相關，且整個 session 中不會變的東西
@@ -41,7 +23,6 @@ class CbzAuth(AuthBase):
 
         # request line
         rline = r.method + ' ' + r.path_url + ' HTTP/1.1'
-        # rline = r.method + ' /v1/orders HTTP/1.1'
 
         # 製作要放入簽章的字串
         sig_str = 'x-date: ' + x_date + '\n' + rline
@@ -53,11 +34,47 @@ class CbzAuth(AuthBase):
         r.headers['X-Date'] = x_date
         r.headers['Authorization'] = auth
         return r
-    
-# 用 Session 送 request
-s = requests.Session() # 建立一個 session 空物件
-s.auth = CbzAuth(username=username, secret=secret)
 
-r = s.get(url, params=payload) # 在 session 物件中送出一個 get request
-print(r.status_code)
-# print(r.text)
+
+# 函式目標：收到頁碼，回傳該頁的資料
+def fetch_page(url, session, page_num):
+    # payload
+    payload = {'page': page_num, 'per_page': 50}
+
+    # 在 session 物件中送出一個 get request
+    r = session.get(url, params=payload) 
+  
+    return r.json()
+
+def fetch_all_orders():
+    # api url
+    url_base = 'https://api.cyberbiz.co'
+    url_path = '/v1/orders'
+    url = url_base + url_path
+    
+    # 存所有資料的 list
+    all_orders = []
+
+    
+
+    # 用 Session 送 request
+    s = requests.Session() # 建立一個 session 空物件
+    s.auth = CbzAuth(username=username, secret=secret)
+    
+    page_num = 1
+
+    while True:
+        order_data = fetch_page(url, s, page_num)
+        # print(len(order_data))
+
+        if order_data:
+            all_orders.extend(order_data)
+            page_num += 1
+        else:
+            break
+    
+    return all_orders
+
+all_orders = fetch_all_orders()
+
+print(len(all_orders))
